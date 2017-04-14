44ebb6fc64d981fef44d8aa8a4c0e14c8cf30053
wendysegura <118@holbertonschool.com>
2017-04-14 23:27:26 +0000

    added more delimiters

--- _strtok.c
+++ _strtok.c
@@ -2,6 +2,8 @@
 
 /**
  * strtok - tokenizes string
+ * @str: user input
+ * @tokens: pointer to array of tokenized input
  * Return: pointer to array of tokens
  */
 char **_strtok(char *str, char **tokens)
@@ -9,13 +11,13 @@ char **_strtok(char *str, char **tokens)
 	char *token;
 	unsigned int i;
 
-	token = strtok(str, " ");
+	token = strtok(str, "\n\t\r ");
 
 	i = 0;
 	while (token != NULL)
 	{
 		tokens[i] = token;
-		token = strtok(NULL, " ");
+		token = strtok(NULL, "\n\t\r ");
 		i++;
 	}
 
