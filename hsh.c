7d1ccb523a4a8864e56f8b365ba4b7eff54f38f6
Carrie Ybay <carrieybay@gmail.com>
2017-04-19 14:34:03 -0700

    Change exit function to _exit syscall for exit builtin

--- hsh.c
+++ hsh.c
@@ -31,7 +31,7 @@ int main(void)
 		if (builtin_status == 0)
 			continue;
 		if (builtin_status == -1)
-			exit(EXIT_SUCCESS);
+			_exit(EXIT_SUCCESS);
 		flag = 0; /* 0 if full_path is not malloc'd */
 		path = _getenv("PATH");
 		fullpath = _which(tokens[0], fullpath, path);
